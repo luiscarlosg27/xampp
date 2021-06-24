511b4f750feadc29bf2742092417f7389ba950c3
luiscarlosg27 <lcgonzaleza@unadvirtual.edu.co>
2021-06-24 14:16:02 -0500

    se actualizan los archivos de javascript

--- htdocs/pueblos-magicos/resources/views/partials/javascript.blade.php
+++ htdocs/pueblos-magicos/resources/views/partials/javascript.blade.php
@@ -1,15 +1,3 @@
-<!-- Javascript -->
-<script src="{{URL:: asset('/bundles/libscripts.bundle.js')}}"></script>
-<script src="{{URL:: asset('/bundles/vendorscripts.bundle.js')}}"></script>
-
-<script src="{{URL:: asset('/bundles/c3.bundle.js')}}"></script>
-<script src="{{URL:: asset('/bundles/chartist.bundle.js')}}"></script>
-<script src="{{URL:: asset('/bundles/knob.bundle.js')}}"></script>
-<script src="{{URL:: asset('/bundles/jvectormap.bundle.js')}}"></script>
-<script src="{{URL:: asset('/vendor/toastr/toastr.js')}}"></script>
-
-<script src="{{URL:: asset('/bundles/mainscripts.bundle.js')}}"></script>
-<script src="{{URL:: asset('/js/index.js')}}"></script>
 
 <!--   Core JS Files   -->
 <script src="URL::  asset('/js/core/jquery.min.js')" type="text/javascript"></script>
