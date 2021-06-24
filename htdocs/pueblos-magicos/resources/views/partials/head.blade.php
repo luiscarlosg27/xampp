fb9a1deae40b98e5c9de1ac8f91d5f830961139d
luiscarlosg27 <lcgonzaleza@unadvirtual.edu.co>
2021-06-24 14:10:41 -0500

    se registran los archivos de javascript y material

--- htdocs/pueblos-magicos/resources/views/partials/head.blade.php
+++ htdocs/pueblos-magicos/resources/views/partials/head.blade.php
@@ -1,21 +1,18 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
-<meta name="description" content="Osam Bootstrap 4x admin is super flexible, powerful, clean &amp; modern responsive admin dashboard with unlimited possibilities.">
-<meta name="keywords" content="admin template, Osam admin template, dashboard template, flat admin template, responsive admin template, web app, Light Dark version">
-<meta name="author" content="GetBootstrap, design by: puffintheme.com">
 
-<link rel="icon" href="favicon.ico" type="image/x-icon">
-<!-- VENDOR CSS -->
-<link rel="stylesheet" href="{{URL::asset('/vendor/bootstrap/css/bootstrap.min.css')}}">
-<link rel="stylesheet" href="{{URL::asset('/vendor/font-awesome/css/font-awesome.min.css')}}">
-<link rel="stylesheet" href="{{URL:: asset('/vendor/animate-css/vivify.min.css')}}">
-
-<link rel="stylesheet" href="{{URL:: asset('/vendor/c3/c3.min.css')}}"/>
-<link rel="stylesheet" href="{{URL:: asset('/vendor/chartist/css/chartist.css')}}">
-<link rel="stylesheet" href="{{URL:: asset('/vendor/chartist-plugin-tooltip/chartist-plugin-tooltip.css')}}">
-<link rel="stylesheet" href="{{URL:: asset('/vendor/toastr/toastr.min.css')}}">
-<link rel="stylesheet" href="{{URL:: asset('/vendor/jvectormap/jquery-jvectormap-2.0.3.css')}}"/>
-
-<!-- MAIN CSS -->
-<link rel="stylesheet" href="{{URL:: asset('/css/site.min.css')}}">
+<!-- MAIN CSS Material KIT -->
+<meta charset="utf-8" />
+<link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="./assets/img/favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Material Kit PRO by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="{{URL:: asset('/css/material-kit.css?v=2.1.1')}}" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="{{URL:: asset('/demo/demo.css')}}" rel="stylesheet" />
+  <link href="{{URL:: asset('/demo/vertical-nav.css')}}" rel="stylesheet" />
