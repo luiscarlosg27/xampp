a6bae057bba248ad2796faf114e7d374d74ed4d2
luiscarlosg27 <lcgonzaleza@unadvirtual.edu.co>
2021-06-24 14:55:03 -0500

    se genera el header se actualiza js y el nav

--- htdocs/pueblos-magicos/resources/views/layouts/app.blade.php
+++ htdocs/pueblos-magicos/resources/views/layouts/app.blade.php
@@ -6,30 +6,12 @@
         </title>
         @include('partials.head')
     </head>
-    <body class="theme-orange box_layout">
+    <body>
     <!-- Page Loader -->
-        <div class="page-loader-wrapper">
-            <div class="loader">
-                <div class="m-t-30"><img src="../assets/images/icon.svg" width="40" height="40" alt="Osam"></div>
-                <p>Please wait...</p>
-            </div>
-        </div>
-    <!-- Theme Setting -->
-        <div class="themesetting">
-            <a href="javascript:void(0);" class="theme_btn"><i class="icon-magic-wand"></i></a>
-            <ul class="choose-skin list-unstyled mb-0">
-                <li data-theme="green"><div class="green"></div></li>
-                <li data-theme="orange" class="active"><div class="orange"></div></li>
-                <li data-theme="blush"><div class="blush"></div></li>
-                <li data-theme="cyan"><div class="cyan"></div></li>
-            </ul>
-        </div>
-    <!-- Overlay For Sidebars -->
-        <div class="overlay">
-            <div id="wrapper">
-                @yield('content')
-            </div>
-        </div>
+    
+         <!-- Header y Nav -->   
+        @include('partials.header')
+    
         <footer></footer>
         @include('partials.javascript')
     </body>
